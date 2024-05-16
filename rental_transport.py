dd5f2ccf4a620c106df6b38ecbd0273b2c42d9dc
floren-audrina <c14210225@john.petra.ac.id>
2024-05-16 15:37:11 +0700

    kerangka

---
+++ rental_transport.py
@@ -0,0 +1,22 @@
+from nameko.rpc import rpc
+
+import dependencies
+
+# class RentalService:
+#      @rpc
+#     def get_car(self):
+
+
+#     @rpc
+#     def get_search(self, num):
+
+    
+#     @rpc
+#     def add_car(self, num, r_type):
+
+    
+#     @rpc
+#     def edit_car(self, num):
+        
+#     @rpc
+#     def del_car(self, num):
