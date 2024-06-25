2c22c8dad030b7a6362f3005d9346285d5d88657
floren-audrina <c14210225@john.petra.ac.id>
2024-06-25 20:53:58 +0700

    -

--- ada_kawan/transport/run.sh
+++ ada_kawan/transport/run.sh
@@ -7,7 +7,7 @@ until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
     sleep 20
 done
 
-# Run the service
-export PYTHONPATH=$(pwd)
+# Set PYTHONPATH to the current working directory
+export PYTHONPATH=$(pwd)/application/ada_kawan/transport
 
 nameko run --config config.yml transport.rental_transport --backdoor 3000
