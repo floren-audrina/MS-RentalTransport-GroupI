aca42f298e3db18c5a7acf2d63526e4269cb3f4f
floren-audrina <c14210225@john.petra.ac.id>
2024-06-26 10:09:30 +0700

    add boto3

--- puri/transport/setup.py
+++ puri/transport/setup.py
@@ -7,12 +7,12 @@ setup(
     description='Rental Car Transportations with Nameko',
     author='nameko',
     packages=find_packages(exclude=['test', 'test.*']),
-    # py_modules=['user'],
     install_requires=[
         "marshmallow==2.19.2",
         "nameko==v3.0.0-rc6",
         "mysqlclient==1.4.6",  # Added MySQL client
-        "mysql-connector-python==8.0.26"
+        "mysql-connector-python==8.0.26",
+        "boto3==1.20.104"  # Added boto3
     ],
     extras_require={
         'dev': [
